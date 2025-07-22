18cee0a684f8bb27652f69601a58102c497478ae
Shea Duma <nu@she-a.eu>
2025-07-22 20:31:15 +0300

    some basic file reading (no security yet)

--- main.c
+++ main.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sqlite3.h>
 
-int openFile(char * filepath){
-    FILE *f = fopen(filepath, "rb");
+
+
+#define BUFFER 256
+
+void sendFail(){
+    puts("HTTP/1.1 404 Not Found\r\n"
+    "Content-Type: text/plain\r\n"
+    "Connection: close\r\n"
+    "\r\n"
+    "token or password are incorrect or sth idk");
+}
+
+int openFile(const char * filepath){
+    FILE *f;
+    if(!filepath)
+        return -1;
+    char fp[BUFFER+1];
+    if(filepath[0] == '/'){
+        strncpy(fp+1, filepath, BUFFER);
+        fp[0] = '.';
+        f = fopen(fp, "rb");
+    }
+    else
+        f = fopen(filepath, "rb");
+    
     if (!f) {
         puts("HTTP/1.1 404 Not Found");
         puts("Content-Type: text/plain\n");
-        puts("file not found");
+        puts("file not found\n");
+        printf("%s",filepath);
         return 1;
     }
     fseek(f, 0, SEEK_END);
@@ -31,13 +56,77 @@ int openFile(char * filepath){
     return 0;
 }
 
+int query(char * key){
+    sqlite3 *db;
+
+    char *zErrMsg = 0;
+    int rc;
+    sqlite3_stmt *stmt;
+    const char *sql = "SELECT password, filename, filepath FROM trans WHERE key = ?;";
+
+    rc = sqlite3_open("trans.db", &db);
+    if( rc ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return -2;
+    }
+
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, 0) != SQLITE_OK) {
+        fprintf(stderr, "failed to prepare: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return -3;
+    }
+
+    if (sqlite3_bind_text(stmt, 1, key, -1, SQLITE_STATIC) != SQLITE_OK) {
+        fprintf(stderr, "failed to bind key\n");
+        sqlite3_finalize(stmt);
+        sqlite3_close(db);
+        return -4;
+    }
+
+    if (sqlite3_step(stmt) != SQLITE_ROW) {
+        sqlite3_close(db);
+        return -5;
+    }
+    const char *password = (const char*)sqlite3_column_text(stmt, 0);
+    const char *filename = (const char*)sqlite3_column_text(stmt, 1);
+    const char *filepath = (const char*)sqlite3_column_text(stmt, 2);
+    sqlite3_close(db);
+
+    //printf("password: %s\nfilename: %s\nfilepath: %s\n", password, filename, filepath);
+    if(!filepath || !filename){
+        return -6;
+    }
+//    if(!password){
+        openFile(filepath);
+  //  }
+    
+
+
+    return 0;
+}
+
 int main(){
-    char *filepath = "./files/uwu.png";
+    char * key;
+    key = (char*) malloc(BUFFER);
+    if(!key){
+        sendFail();
+        return 0;
+    }
+    strncpy(key, getenv("REQUEST_URI"), BUFFER); 
+    for(int i = 0; i < strlen(key); i++)
+        if(key[i] < 43 || key[i] > 122){
+            free(key);
+            sendFail();
+            return 0;
+        }
+    if(query(key)){
+        free(key);
+        sendFail();
+        return 0;
+    }
 
-    if(openFile(filepath) != 0)
-        return -1;
-
-//    puts("Content-Type: text/plain\n\n");
-//    puts("test\n");
+    //char *filepath = "./files/uwu.png";
+    //if(openFile(filepath) != 0) return -1;
+    free(key);
     return 0;
 }
--- makefile
+++ makefile
@@ -1,10 +1,10 @@
 all:
 	rm fileServer || true
-	gcc -o fileServer main.c
+	gcc -o fileServer main.c -lsqlite3
 
 test: all
-	SERVER_NAME="she-a.eu" REQUEST_URI="/path/subpath" REMOTE_ADDR="127.0.0.1" ./fileServer
-test-path: all
-	SERVER_NAME="she-a.eu" REQUEST_URI="/path/subpath" REMOTE_ADDR="127.0.0.1" ./fileServer
+	SERVER_NAME="she-a.eu" REQUEST_URI="test" REMOTE_ADDR="127.0.0.1" ./fileServer
+test-gratis: all
+	SERVER_NAME="she-a.eu" REQUEST_URI="gratis" REMOTE_ADDR="127.0.0.1" ./fileServer
 test-subdomain: all
 	SERVER_NAME="subdomain.example.com" REQUEST_URI="/debug" REMOTE_ADDR="127.0.0.1" ./fileServer
